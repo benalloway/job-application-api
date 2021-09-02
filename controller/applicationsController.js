35b4066546ed1c622f6404bc1c9dbc4f0a784055
Benjamin Alloway <benalloway@gmail.com>
2021-09-02 15:32:55 -0400

    updated controller qualifying applications function to make it more efficient.

--- controller/applicationsController.js
+++ controller/applicationsController.js
@@ -6,7 +6,7 @@ const supabaseKey = process.env.SUPABASE_PUBLIC_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 // DB design:
-// (table_1) job_listing: id, acceptible answers
+// (table_1) job_listing: id, acceptible answers, title
 // (table_2) applications: id, qualified, name, email, questions, job_listing__id
 
 //
@@ -31,75 +31,50 @@ export const addApplication = async (request, reply) => {
     const job_listing_id = jobListingId ?? "e84f7e28-c8c7-4588-8f81-e2a51e776564"
 
     // grab acceptible answers for this job-listing from the DB
-    try {
-        const {data, error} = await supabase
-                .from('job_listings')
-                .select('acceptible_answers')
-                .eq('id', job_listing_id)
-                .single()
-        
-        console.log("returned data from job_listing.acceptible_answers", data)
-        
-        if(error) return {data: null, error}
+    const {data, error} = await supabase
+            .from('job_listings')
+            .select('acceptible_answers')
+            .eq('id', job_listing_id)
+            .single()
     
-        // check if the application meets min. qualifications based on Acceptible Answers.
-        isQualified = isQualifiedApplication(questions, data.acceptible_answers)
+    if(error) return {data: null, error}
 
-    } catch(error) {
-        return {data: null, error}
-    }
+    // check if the application meets min. qualifications based on Acceptible Answers.
+    isQualified = validate(questions, data.acceptible_answers)
 
     // Save application to DB with proper qualified status
-    try {
-        const { data, error } = await supabase
-            .from('applications')
-            .insert([
-            { qualified: isQualified, job_listing_id, name, email, questions },
-        ])
-    
-        return {data, error}
+    const { data, error } = await supabase
+        .from('applications')
+        .insert([
+        { qualified: isQualified, job_listing_id, name, email, questions },
+    ])
 
-    } catch(error) {
-        return {data: null, error}
-    }
+    return {data, error}
 
 }
 
-// 
-// Takes just the questions that matter for qualification purposes, and determines if the submitted application meets min. qualifications.
-// step 1. filter out non-qualifying questions
-// step 2. filter out qualifying questions that are answered correctly
-// step 3. if there are no failed questions - returns true.
-function isQualifiedApplication(questions, acceptibleAnswers) {
-    console.log("questions", questions)
-    console.log("acceptible anwers", acceptibleAnswers)
+// validates incoming application: if it is not a valid application it will be rejected.
+function validate(questions, answers) {
 
-    if(Array.isArray(questions) && questions.length > 0) {
-        
-        // first check is to make sure we have the right amount of acceptible questions - if not return false
-        const acceptibleQuestions = questions.filter(question => acceptibleAnswers.find(answer => answer.Id === question.Id))
-
-        // quickly check to make sure we have the min. amount of acceptible answers for this job listing.
-        // TODO: maybe enhance this to make sure acceptible answers coming through is 1:1 with acceptible answers in db? (like each individual one)
-        if(acceptibleAnswers?.length !== acceptibleQuestions?.length) return false
-
-        // keep track of any qualifying questions that fail
-        const failedQuestions = questions.filter(question => {
-            // check if this question is a qualifying question
-            const qualifyingQuestion = acceptibleAnswers.find(answer => answer.Id === question.Id)
-            
-            // filter out if it is not a qualifying question
-            if (!qualifyingQuestion) {
-              return false
-            }
-          
-            // filter out correctly answered qualifying questions - leaving just failed questions.
-            return qualifyingQuestion.Answer !== question.Answer
-        })
-        
-        // if there are no failed questions: Bob's your uncle!
-        return failedQuestions.length === 0
-    } else {
-        throw("Something went wrong in qualifyApplication")
+    // Make sure things are in order
+    if(!Array.isArray(questions) || !questions.length > 0) {
+        console.log("There is an issue with the Incoming questions ")
     }
-}
+    if(!Array.isArray(answers) || !answers.length > 0) {
+        console.log("There is something wrong with the acceptible answers")
+    }
+
+    let valid = true
+
+    // checks each question against the acceptible answer, if ANY of them are false stop loop and return false.
+    for(let x=0; x < questions.length; x++) {
+      let matched = answers.find(ans => ans.Id === questions[x].Id)
+      if(!matched) continue
+
+      if(matched.Answer !== questions[x].Answer) {
+        valid = false
+        break
+      }
+    }
+    return valid
+  }
